3cb6636e5f3dcc748b52188135ef36ac34bef826
DShomin <lhm53779354@gmail.com>
2022-02-20 18:34:59 +0900

    3.7 Structs with Pointers

--- main.go
+++ main.go
@@ -2,26 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	"github.com/DShomin/lomitcoin/person"
 )
 
-// create class
-type person struct {
-	name string
-	age  int
-}
-
-// create class method
-func (p person) sayHello() {
-	fmt.Printf("Hello! My name is %s and I'm %d\n", p.name, p.koreanAge())
-}
-
-func (p person) koreanAge() int {
-	currentYear := int(time.Now().Year())
-	return currentYear - p.age + 1
-}
-
 func main() {
-	lomit := person{name: "Lomit", age: 1993}
-	lomit.sayHello()
+	lomit := person.Person{}
+	lomit.SetDetails("lomit", 30)
+	fmt.Println(lomit)
 }
